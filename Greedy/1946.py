62be69408970c80573ed727990ead52ac76b2400
imsosleepy <kimdongha@naver.com>
2020-10-04 18:48:03 +0900

    modify 1931

--- Greedy/1946.py
+++ Greedy/1946.py
@@ -14,21 +14,18 @@ if __name__ =="__main__":
         score = []
         count = 1
         for j in range(N):
-           score.append(list(map(int, sys.stdin.readline().split())))
+           score.append(list(map(int, sys.stdin.readline().strip().split())))
 
         score.sort()
 
         low_score = score[0][1]
-        pass_list = []
-        for j in range(N):
-            if low_score >= score[j][1]:
-                pass_list.append(score[j])
 
-        for j in range(0,len(pass_list)):
-            if low_score > pass_list[j][1]:
+        for j in range(0,len(score)):
+            if low_score < score[j][1]:
+                continue
+            if low_score > score[j][1]:
                 count+=1
-                low_score = pass_list[j][1]
+                low_score = score[j][1]
 
-        #print(pass_list)
         print(count)
 
